923ea9964da1acc898767a2553f79bcc014b9c97
cayek <cayek@timc-bcm-15.imag.fr>
2015-06-09 17:35:16 +0200

    R examples

---
+++ examples/findStructure.R
@@ -0,0 +1,40 @@
+source("/home/cayek/Projects/TESS3/src/Rwrapper/TESS3.R")
+library(LEA)
+
+setwd( "/home/cayek/Projects/TESS3/data/simulated/Athaliana" )
+###########################################################################
+# Run TESS3 on a data set simualted from an Arabidopsis Athalina data set #
+###########################################################################
+
+#read data
+spatialData = read.coord("Athaliana.coord")
+n = nrow(spatialData)
+
+project = TESS3( genotype = "Athaliana.geno", 
+                 spatialData = "Athaliana.coord", 
+                 K = 1:5, 
+                 ploidy = 1, 
+                 rep = 1, 
+                 maskedProportion = 0.2)
+
+
+###########################################
+# Chose of K with cross-entropy criterion #
+###########################################
+
+plot( 1:5, getCrossEntropy( project ), main  = "Cross entropy",type="b", xlab = "K", ylab = "corss entropy" )
+
+################################
+# Plot result on map for K = 3 #
+################################
+
+source("/home/cayek/Téléchargements/R/POPSutilities.r")
+
+asciiFile="/home/cayek/Projects/TESS3/data/simulated/Athaliana/down_etopo1.asc"
+grid=createGridFromAsciiRaster(asciiFile)
+# To display only altitudes above 0:
+constraints=getConstraintsFromAsciiRaster(asciiFile,cell_value_min=0)
+
+maps(matrix = getQ( project, K = 3 ),
+     coord = spatialData,
+     grid=grid,constraints=constraints,method="max",main="ancestry coefficient with K = 3")
--- src/Rwrapper/TESS3.R
+++ src/Rwrapper/TESS3.R
@@ -8,7 +8,7 @@ TESS3_cmd <- "/home/cayek/Projects/TESS3/build/TESS3"
 
 ##########################################################################
 
-TESS3 <- function( genotype,  spatialData, K, ploidy=1, seed=0 ) {
+TESS3 <- function( genotype,  spatialData, K, ploidy=1, seed=-1, rep = 1, maskedProportion = 0.0, alpha = 0.001 ) {
   
   #test if we can found sNMF
   if (!(class(TESS3_cmd)=="character")) {
@@ -25,33 +25,236 @@ TESS3 <- function( genotype,  spatialData, K, ploidy=1, seed=0 ) {
   }
   TESS3_workingDirectory = paste( getwd(), "/TESS3_workingDirectory/", sep="" )
   
-  cat("---------------------WRITTING DATA-----------------------\n")
-  #write genotype and spatial data into file
-  genotype_file  = paste( TESS3_workingDirectory, "/aux.geno", sep="" )
-  write.table(file = genotype_file,
-              t(genotype), row.names = F, col.names = F, quote = F, sep = "")
-  spatialData_file  = paste( TESS3_workingDirectory, "/aux.coord", sep="" )
-  write.table(file = spatialData_file,
-              spatialData, row.names = F, col.names = F, quote = F, sep = " ")
+  if( class( genotype ) == "matrix" ) {
+    
+    #cat("---------------------WRITTING GENOTYPE-----------------------\n")
+    #write genotype and spatial data into file
+    genotype_file  = paste( TESS3_workingDirectory, "/aux.geno", sep="" )
+    write.table(file = genotype_file,
+                t(genotype), row.names = F, col.names = F, quote = F, sep = "")
+    
+  } else if( class( genotype ) == "character" ) {
+    
+    genotype_file = genotype
+    
+  }
   
-  #launch TESS3 
-  cat("---------------------RUNNING TESS3-----------------------\n")
-  system( paste( TESS3_cmd, 
-                 "-x", genotype_file,
-                 "-r", spatialData_file,
-                 "-K", K,
-                 "-m", ploidy,
-                 "-s", seed,
-                 "-I",#to init Q with a sample of locus
-                 sep= " " ) )
+  if( class( spatialData ) == "matrix" ) {
+    
+    #cat("---------------------WRITTING GENOTYPE-----------------------\n")
+    #write genotype and spatial data into file
+    spatialData_file  = paste( TESS3_workingDirectory, "/aux.coord", sep="" )
+    write.table(file = spatialData_file,
+                spatialData, row.names = F, col.names = F, quote = F, sep = " ")
+    
+  } else if( class( spatialData ) == "character" ) {
+    
+    spatialData_file  = spatialData
+    
+  }
   
+  #optionnal arg
+  optionalArg = ""
+  if( seed > 0 ) {
+    
+    optionalArg = paste( optionalArg, "-s", seed )
+    
+  }
+  if( maskedProportion > 0.0 ) {
+    
+    optionalArg = paste( optionalArg, "-c", maskedProportion )
+    
+  }
   
-  #read result
-  cat("---------------------READING RESULTS-----------------------\n")
-  res = list()
-  res$Q = as.matrix(read.table(file = paste( TESS3_workingDirectory,"/aux" ,".",K,".Q",sep="")))
-  res$G = as.matrix(read.table(file = paste( TESS3_workingDirectory,"/aux" ,".",K,".Q",sep="")))
+  res = vector("list",length=length(K))
+  attr(res,"K") = K
+  
+  for( k in 1:length(K)) {
+    
+    res[[k]]$Q = vector("list",length=rep) 
+    res[[k]]$G = vector("list",length=rep) 
+    res[[k]]$Fst = vector("list",length=rep)
+    res[[k]]$error = 1:rep
+    res[[k]]$all.ce = 1:rep
+    res[[k]]$masked.ce = 1:rep
+
+    output_Q = paste( TESS3_workingDirectory,"/aux" ,".",K[k],".Q",sep="")
+    output_G = paste( TESS3_workingDirectory,"/aux" ,".",K[k],".G",sep="")
+    output_Fst = paste( TESS3_workingDirectory,"/aux" ,".",K[k],".Fst",sep="")
+    output_summary = paste( TESS3_workingDirectory,"/aux" ,".",K[k],".sum",sep="")
+    
+    for( r in 1:rep) {
+      
+      #launch TESS3 
+      cat("---------------------RUNNING TESS3-----------------------\n")
+      system( paste( TESS3_cmd, 
+                     "-x", genotype_file,
+                     "-r", spatialData_file,
+                     "-K", K[k],
+                     "-m", ploidy,
+                     "-I",#to init Q with a sample of locus
+                     "-q",output_Q,
+                     "-g",output_G,
+                     "-f",output_Fst,
+                     "-y",output_summary,
+                     "-a", alpha,
+                     optionalArg,
+                     sep= " " ) )
+      
+      
+      #read result
+      cat("---------------------READING RESULTS-----------------------\n")
+      res[[k]]$Q[[r]] = as.matrix(read.table(file = output_Q))
+      res[[k]]$G[[r]] = as.matrix(read.table(file = output_G))
+      res[[k]]$Fst[[r]] = as.matrix(read.table(file = output_Fst))
+      aux = readLines(output_summary)
+      res[[k]]$error[r] = as.numeric( sub( ".*: ","", aux[1] ) )
+      res[[k]]$all.ce[r] = as.numeric( sub( ".*: ","", aux[2] ) )
+      res[[k]]$masked.ce[r] = as.numeric( sub( ".*: ","", aux[3] ) )
+    }
+    
+    
+  }
+  
   
   return(res)
   
 }
+
+
+##########################################################################
+##########################Result Getter###################################
+##########################################################################
+
+getQ <- function( project, K, run = "best" ) {
+  
+  k = which(attr(project, "K") == K )
+  
+  if( length(k) == 0 ) {
+    
+    stop(paste("There is no run for K =",K,"!"))
+    
+  } else {
+    k=k[1]
+    if (class(run) == "numeric") {
+      
+      return( project[[k]]$Q[[run]] )
+      
+    } else if( run  == "best"  ) {
+      
+      best = which.min( project[[k]]$error )
+      
+      return( project[[k]]$Q[[ best ]] )
+      
+    }
+  }
+}
+
+getG <- function( project, K, run = "best" ) {
+  
+  k = which(attr(project, "K") == K )
+  
+  if( length(k) == 0 ) {
+    
+    stop(paste("There is no run for K =",K,"!"))
+    
+  } else {
+    k=k[1]
+    if (class(run) == "numeric") {
+      
+      return( project[[k]]$G[[run]] )
+      
+    } else if( run  == "best"  ) {
+      
+      best = which.min( project[[k]]$error )
+      
+      return( project[[k]]$G[[ best ]] )
+      
+    }
+  }
+}
+
+getFst <- function( project, K, run = "best" ) {
+  
+  k = which(attr(project, "K") == K )
+  
+  if( length(k) == 0 ) {
+    
+    stop(paste("There is no run for K =",K,"!"))
+    
+  } else {
+    k=k[1]
+    if (class(run) == "numeric") {
+      
+      return( project[[k]]$Fst[[run]] )
+      
+    } else if( run  == "best"  ) {
+      
+      best = which.min( project[[k]]$error )
+      
+      return( project[[k]]$Fst[[ best ]] )
+      
+    }
+  }
+}
+
+getCrossEntropy <- function( project, func = mean  ) {
+  
+  res = c()
+  
+  for( k in 1:length(project) ) {
+    
+    res = c(res, func( project[[k]]$masked.ce ) ) 
+    
+  }
+  
+  return(res)
+  
+}
+
+getError <- function( project, func = mean  ) {
+  
+  res = c()
+  
+  for( k in 1:length(project) ) {
+    
+    res = c(res, func( project[[k]]$error ) ) 
+    
+  }
+  
+  return(res)
+  
+}
+
+##########################################################################
+################################Reader####################################
+##########################################################################
+
+read.coord <- function( file ) {
+  
+  if (file.exists( file = file )) {
+    
+    return(as.matrix(read.table( file = file )))
+    
+  } else {
+    
+    stop(paste("Can not find the file:", file))
+  }
+  
+}
+
+read.tess <- function( file ) {
+  
+  
+  if (file.exists( file = file )) {
+    
+    return(as.matrix(read.table( file = file )))
+    
+  } else {
+    
+    stop(paste("Can not find the file:", file))
+  }
+  
+  
+}
+
