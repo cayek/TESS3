7f2e6b3d8412b5acb52c2ced326fa11ae4607ec3
cayek <cayek@timc-bcm-15.imag.fr>
2015-06-15 14:24:24 +0200

    script to install R wrapper

---
+++ installRwrapper.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+usage(){
+	echo "Usage: $0 [-t | --tess3 TESS3_program] "
+	exit 1
+}
+
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    -t|--tess3)
+    tess3="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+#tess3 project directory
+dir=`pwd`
+
+#tess3 cmd line 
+if [ -z "$tess3" ]; then 
+    tess3="./build/TESS3"
+fi
+
+#convert relative path into absolute path
+f=`dirname $tess3`
+tess3_absolute=`cd $f; pwd`"/"`basename $tess3`
+
+#check if tess3 program exist
+if [ ! -e "$tess3_absolute" ]; then 
+    echo "Can not find TESS3 program : $tess3_absolute file do not exist !" 1>&2
+    usage
+fi
+
+#check if tess3 program can be executed
+if [ ! -x "$tess3" ]; then 
+    echo "Can not execute TESS3 program : $tess3_absolute is not executable" 1>&2
+    usage
+fi
+
+#find Rwrapper and example 
+Rwrapper_path="$dir/src/Rwrapper/TESS3.R"
+findSelection_path="$dir/examples/findSelection.R"
+findStructure_path="$dir/examples/findStructure.R"
+Athalina_path="$dir/data/simulated/Athaliana"
+if [ ! -e "$Athalina_path" ] || [ ! -e "$Rwrapper_path" ] || [ ! -e "$findSelection_path" ] || [ ! -e "$findStructure_path" ]; then 
+    echo "Can not find find R sources !" 1>&2
+    echo "Be sure that you run `basename $0` in the TESS3 project directory" 1>&2
+fi
+
+
+sed -i "s|^[ ]*Tess3wrapper.dirrectory[ ]*<-.*|Tess3wrapper.dirrectory <- \"$Rwrapper_path\"|g" $findSelection_path
+sed -i "s|^[ ]*Athaliana.dirrectory[ ]*<-.*|Athaliana.dirrectory <- \"$Athalina_path\"|g" $findSelection_path
+
+sed -i "s|^[ ]*Tess3wrapper.dirrectory[ ]*<-.*|Tess3wrapper.dirrectory <- \"$Rwrapper_path\"|g" $findStructure_path
+sed -i "s|^[ ]*Athaliana.dirrectory[ ]*<-.*|Athaliana.dirrectory <- \"$Athalina_path\"|g" $findStructure_path
+
+sed -i "s|^[ ]*TESS3.cmd[ ]*<-.*|TESS3.cmd <- \"$tess3_absolute\"|g" $findStructure_path
