22f8df943f611d2c5039061c948a5975aa497850
cayek <cayek@timc-bcm-15.imag.fr>
2015-06-18 16:57:12 +0200

    only for develop branch stuff

---
+++ release.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# TESS3 directory on my linux computer
+cd /home/cayek/Projects/TESS3
+ROUGE="\\033[1;31m"
+dir_TESS3=`pwd`
+
+function test {
+    eval "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo -e "$ROUGE" "error with $1" >&2
+	exit 1
+    fi
+    return $status
+}
+
+
+###############################
+# push all changes to develop #
+###############################
+git checkout develop
+# check if there are not commited file
+status=`git status 2>&1 | tee`
+dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
+newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
+renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
+deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
+
+if [ "${dirty}" == "0" ] || [ "${newfile}" == "0" ] || [ "${renamed}" == "0" ] || [ "${deleted}" == "0" ]; then
+echo -e "$ROUGE" "commit in develop branch before release"
+exit 1
+fi
+
+# compile documentation
+# cd doc/src/
+# test "latex note.tex &> /dev/null"
+# test "bibtex note &> /dev/null"
+# test "latex note.tex &> /dev/null"
+# test "latex note.tex &> /dev/null"
+# test "dvipdf note.dvi &> /dev/null"
+# rm -f ../documentation.pdf
+# cp -f note.pdf ../documentation.pdf 
+# git add ../documentation.pdf
+# git commit -m "documentation"
+# cd "$dir_TESS3"
+
+#git push
+git push
+
+#################
+# try to deploy #
+#################
+
+cd ~/Téléchargements/
+
+rm -rf TESS3_testdeploy
+git clone ssh://cayek@patator.imag.fr/home/cayek/noBackup/TESS3.git TESS3_testdeploy
+cd TESS3_testdeploy/
+git checkout develop
+
+mkdir build
+cd build
+test "cmake -DCMAKE_BUILD_TYPE=release ../ &> /dev/null"
+test "make TESS3 &> /dev/null"
+cd ../
+test "./setupRsrc &> /dev/null"
+
+#############
+# run tests #
+#############
+
+test "Rscript test/scriptR/Rtest.R  &> /dev/null"
+
+cd ~/Téléchargements/
+rm -rf TESS3_testdeploy
+
+#################
+# if ok release #
+#################
+cd "$dir_TESS3"
+
+git checkout master
+git merge develop
+
+#push on github
+ssh cayek@patator.imag.fr <<EOF
+cd noBackup/TESS3.git
+git push 
+logout
+EOF
+git checkout develop
