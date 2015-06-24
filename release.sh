5a4dc9fffa54e8a9f0cbc5c2ec929fb9a1b85a30
cayek <cayek@timc-bcm-15.imag.fr>
2015-06-24 09:26:47 +0200

    release.sh again

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ NORMAL="\\033[0;39m"
 dir_TESS3=`pwd`
 
 function test {
-    res="$(eval "$@")"
+    res="$(eval "$@ 2>&1")"
     local status=$?
     if [ $status -ne 0 ]; then
         echo -e "$ROUGE" "error with $1 : " >&2
